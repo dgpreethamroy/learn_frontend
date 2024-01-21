8dafde4595e90abb9678465921a70357c5c77493
CS15B009 <tcs15b009@iittp.ac.in>
2024-01-21 13:31:16 +0530

    Update Message.js

--- frontend/src/components/chat/Message.js
+++ frontend/src/components/chat/Message.js
@@ -1,18 +1,18 @@
 import { format } from "timeago.js";
-import React, { Component }  from 'react';
-
+import React, { Component } from "react";
+import { useState } from "react";
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Message({ message, self }) {
-    let color = "";
+  let color = "";
   if (message.check == "No Hate and Offensive") {
-    color = "clr-green";
+    color = "btn_white";
   } else if (message.check == "Hate Speech") {
-    color = "clr-red";
+    color = "btn_white_text";
   } else {
-    color = "clr-yellow";
+    color = "btn_white_textr";
   }
   return (
     <>
@@ -26,8 +26,8 @@ export default function Message({ message, self }) {
           <div
             className={classNames(
               self !== message.sender
-                ? "text-gray-700 dark:text-gray-400 bg-white border border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700"
-                : "bg-blue-600 dark:bg-blue-500 text-white",
+                ? `${color} dark:${color} bg-white border border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700`
+                : `bg-blue-600 dark:bg-blue-500 ${color}`,
               "relative max-w-xl px-4 py-2 rounded-lg shadow"
             )}
           >
